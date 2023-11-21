8be2f931479d380f28e9c453f63df169e4a78602
arinugraha11 <ari.nugraha@tokopedia.com>
2023-11-21 22:52:19 +0700

    feat: implement search blog

--- lib/src/features/Blog/blog.dart
+++ lib/src/features/Blog/blog.dart
@@ -1,25 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:submission_1/src/features/Blog/card_blog.dart';
 import 'package:submission_1/src/features/Blog/constants.dart';
+import 'package:submission_1/src/features/Blog/models.dart';
 import 'package:submission_1/src/features/Blog/page_scaffold.dart';
 
-class BlogPage extends StatelessWidget {
+class BlogPage extends StatefulWidget {
   const BlogPage({super.key});
+
+  @override
+  State<BlogPage> createState() => _BlogPageState();
+}
+
+class _BlogPageState extends State<BlogPage> {
+  late TextEditingController _textEditingController;
+  late List<BlogData> filteredBlog = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = TextEditingController();
+    filteredBlog = BlogConstants.listBlog;
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  void _filterList(String keyword) {
+    setState(() {
+      if (keyword.isEmpty) {
+        filteredBlog = BlogConstants.listBlog;
+      } else {
+        filteredBlog = BlogConstants.listBlog
+            .where((blog) =>
+                blog.title.toLowerCase().contains(keyword.toLowerCase()))
+            .toList();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlogListScaffold(
-      body: ListView.builder(
-        itemCount: BlogConstants.listBlog.length,
-        itemBuilder: (context, index) {
-          final blogData = BlogConstants.listBlog[index];
-          return CardBlog(
-            imgUrl: blogData.img,
-            title: blogData.title,
-            description: blogData.description,
-            date: blogData.date,
-          );
-        },
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: TextField(
+              decoration: const InputDecoration(
+                hintText: 'Cari Blog',
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _filterList(value);
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredBlog.length,
+              itemBuilder: (context, index) {
+                final blogData = filteredBlog[index];
+                return CardBlog(
+                  imgUrl: blogData.img,
+                  title: blogData.title,
+                  description: blogData.description,
+                  date: blogData.date,
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
